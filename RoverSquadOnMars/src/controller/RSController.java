1bcbfb4a30dba8d9e6395777ab654f94da2f020d
Manda <Manda@mahoni.fritz.box>
2017-04-15 15:54:43 +0200

    initial RoverSquad project

---
+++ RoverSquadOnMars/src/controller/RSController.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class RSController {
+
+	
+	/**
+	 * main method
+	 * initializing plateau
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+	/**
+	 * initialize 
+	 * Rovers
+	 */
+	public void initialize(){
+		
+	}
+}
---
+++ RoverSquadOnMars/src/model/Rover.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Rover {
+
+	
+	public Rover() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private void SetPosition(String lineOne){
+		
+	}
+	
+	private void SetInstructions(String lineTwo){
+		
+	}
+}
