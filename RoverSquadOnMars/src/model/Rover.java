8c216328ed5afe4b28818cbd8beb89e2ee947d74
MAHONI\Manda <marina.stuerboeth@gmail.com>
2017-04-28 19:56:20 +0200

    initial commit

--- RoverSquadOnMars/src/model/Rover.java
+++ RoverSquadOnMars/src/model/Rover.java
@@ -1,17 +1,33 @@
 package model;
 
-public class Rover {
+public class Rover implements Vehicle{
 
 	
 	public Rover() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	private void SetPosition(String lineOne){
+	
+	
+	private void setPosition(String lineOne){
 		
 	}
 	
-	private void SetInstructions(String lineTwo){
+	private void setInstructions(String lineTwo){
+		
+	}
+
+
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turn() {
+		// TODO Auto-generated method stub
 		
 	}
 }
