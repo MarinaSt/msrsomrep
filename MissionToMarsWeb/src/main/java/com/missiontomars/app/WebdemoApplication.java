63d4b9cfc22ce830fa3600b6a8082a6ca0747ea7
MAHONI\Manda <marina.stuerboeth@gmail.com>
2017-04-29 12:55:15 +0200

    basic

--- MissionToMarsWeb/src/main/java/com/missiontomars/app/WebdemoApplication.java
+++ MissionToMarsWeb/src/main/java/com/missiontomars/app/WebdemoApplication.java
@@ -2,10 +2,20 @@ package com.missiontomars.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class WebdemoApplication {
 
+	
+	@ResponseBody
+	@RequestMapping("/user")
+	public String userAuth(){
+		return "User";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WebdemoApplication.class, args);
 	}
