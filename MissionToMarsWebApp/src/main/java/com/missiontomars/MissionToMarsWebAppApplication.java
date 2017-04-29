fd619da4c4bca53a7cfbd44f8f0b499f89988a37
MAHONI\Manda <marina.stuerboeth@gmail.com>
2017-04-29 18:06:27 +0200

    Runs and returns json via REST

--- MissionToMarsWebApp/src/main/java/com/missiontomars/MissionToMarsWebAppApplication.java
+++ MissionToMarsWebApp/src/main/java/com/missiontomars/MissionToMarsWebAppApplication.java
@@ -4,14 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan({"controller", "service"})
-@Controller
+@ComponentScan({"com.missiontomars.controller", "com.missiontomars.service"})
 public class MissionToMarsWebAppApplication {
 
 	public static void main(String[] args) {
