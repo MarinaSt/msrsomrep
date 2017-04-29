aa7ea4e3633a32b410553e63dd009325e6995f51
MAHONI\Manda <marina.stuerboeth@gmail.com>
2017-04-29 17:35:15 +0200

    working version of simplest webapp

--- MissionToMarsWebApp/src/main/java/com/missiontomars/MissionToMarsWebAppApplication.java
+++ MissionToMarsWebApp/src/main/java/com/missiontomars/MissionToMarsWebAppApplication.java
@@ -1,12 +1,26 @@
 package com.missiontomars;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan	
+@Controller	
 public class MissionToMarsWebAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MissionToMarsWebAppApplication.class, args);
+	}
+	
+	@ResponseBody
+	@RequestMapping("/")
+	String entry(){
+		return "home";
 	}
 }
